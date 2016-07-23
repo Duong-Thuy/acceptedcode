58f174209b1bb451212a6832a93ead49aaff3632
giaosudauto <giaosudauto.omega@gmail.com>
2016-07-23 23:36:54 +0000

    Update NK05ORDR.cpp

--- NK05ORDR.cpp
+++ NK05ORDR.cpp
@@ -1,33 +1,26 @@
+/*__ _(_) __ _  ___  ___ _   _  __| | __ _ _   _| |_ ___  
+ / _` | |/ _` |/ _ \/ __| | | |/ _` |/ _` | | | | __/ _ \ 
+| (_| | | (_| | (_) \__ \ |_| | (_| | (_| | |_| | || (_) |
+ \__, |_|\__,_|\___/|___/\__,_|\__,_|\__,_|\__,_|\__\___/ 
+ |___/                                  Accepted Code  */
 #include <bits/stdc++.h>
 using namespace std;
 
-int64_t mu(int64_t a, int64_t b)
-{
-    int res=1;
-    for (int i=1; i<=b; i++)
-        res*=a;
-    return res;
-}
-
 int main()
 {
-#ifdef gsdt
-    freopen("input.txt","r",stdin);
-#endif // gsdt
-
     int T;
     scanf("%d",&T);
     while(T--)
     {
-        int64_t k,m;
+        int k,m;
         cin>>k>>m;
 
-        int64_t a=k;
-        int64_t pos=0;
+        int a=k;
+        int pos=0;
 
         while(1)
         {
-            pos+=a-mu(10,int(log10(a)))+1;
+            pos+=a-pow(10,int(log10(a)))+1;
             a/=10;
             if(a==0)break;
         }
@@ -38,10 +31,10 @@ int main()
         }
         if(m==pos)
         {
-            cout<<k<<endl;
+            printf("%d\n",k);
             continue;
         }
-        if(k==mu(10,int(log10(k))))
+        if(k==pow(10,int(log10(k))))
         {
             printf("0\n");
             continue;
@@ -49,25 +42,23 @@ int main()
         a=k-1;
         while(1)
         {
-            if(pos+a*10+9-mu(10,int(log10(a*10+9)))+1<=m)
+            if(pos+a*10+9-pow(10,int(log10(a*10+9)))+1<=m)
             {
                 a=a*10+9;
-                pos+=a-mu(10,int(log10(a)))+1;
+                pos+=a-pow(10,int(log10(a)))+1;
                 if(pos==m)
                 {
-                    cout<<a<<endl;
+                    printf("%d\n",a);
                     break;
                 }
             }
             else
             {
-                a=mu(10,int(log10(a*10)))+m-pos-1;
-                cout<<a<<endl;
+                a=pow(10,int(log10(a*10)))+m-pos-1;
+                printf("%d\n",a);
                 break;
             }
         }
     }
-
     return 0;
 }
-
